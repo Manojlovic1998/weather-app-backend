2a8bb1968ad7ddcc9f66aeacffadbecea2f064e6
Nemanja Manojlovic <manojlovic911@hotmail.com>
2024-07-21 17:45:47 +0200

    Add controller routes and service methods for fetching weather data

--- src/weather/weather.controller.ts
+++ src/weather/weather.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { WeatherService } from './weather.service';
+import { GetRealtimeDto } from 'src/common/dtos/get-realtime.dto';
+import { GetForecastDto } from 'src/common/dtos/get-forecast.dto';
 
 @Controller('weather')
-export class WeatherController {}
+export class WeatherController {
+  constructor(private readonly weatherService: WeatherService) {}
+
+  @Get('realtime')
+  async getRealtime(@Query() getRealtimeDto: GetRealtimeDto) {
+    return this.weatherService.getRealtimeWeather(getRealtimeDto);
+  }
+
+  @Get('forecast')
+  async getForecast(@Query() getForecastDto: GetForecastDto) {
+    return this.weatherService.getForecastWeather(getForecastDto);
+  }
+}
--- src/weather/weather.service.ts
+++ src/weather/weather.service.ts
@@ -1,6 +1,43 @@
-import { Injectable, HttpService } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { lastValueFrom } from 'rxjs';
 import { GetRealtimeDto } from 'src/common/dtos/get-realtime.dto';
 import { GetForecastDto } from 'src/common/dtos/get-forecast.dto';
 
 @Injectable()
-export class WeatherService {}
+export class WeatherService {
+  private readonly apiKey = process.env.WEATHER_API_KEY;
+  private readonly baseUrl = process.env.WEATHER_API_URL;
+
+  constructor(private readonly httpService: HttpService) {}
+
+  async getRealtimeWeather(getRealtimeDto: GetRealtimeDto) {
+    const { location } = getRealtimeDto;
+    const url = `${this.baseUrl}/current.json?key=${this.apiKey}&q=${location}`;
+
+    try {
+      const response = await lastValueFrom(this.httpService.get(url));
+      return response.data;
+    } catch (error) {
+      throw new HttpException(
+        'Failed to fetch real-time weather data',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async getForecastWeather(getForecastDto: GetForecastDto) {
+    const { location, days } = getForecastDto;
+    const url = `${this.baseUrl}/forecast.json?key=${this.apiKey}&q=${location}&days=${days}`;
+
+    try {
+      const response = await lastValueFrom(this.httpService.get(url));
+      return response.data;
+    } catch (error) {
+      throw new HttpException(
+        'Failed to fetch forecast weather data',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+}
