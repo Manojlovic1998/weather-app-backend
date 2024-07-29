352afd36192152677cdbcd2194d830fc75b7b169
Nemanja Manojlovic <manojlovic911@hotmail.com>
2024-07-29 11:47:57 +0200

    Add temp wild card for origin

--- src/weather/weather.service.spec.ts
+++ src/weather/weather.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { WeatherService } from './weather.service';
+import { HttpService } from '@nestjs/axios';
 
 describe('WeatherService', () => {
   let service: WeatherService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [WeatherService],
+      providers: [
+        WeatherService,
+        {
+          provide: HttpService,
+          useValue: {
+            get: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<WeatherService>(WeatherService);
