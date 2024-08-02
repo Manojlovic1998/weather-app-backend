10f240b58c5185c9c600cc14b0e0ab56599a5bbd
Nemanja Manojlovic <manojlovic911@hotmail.com>
2024-08-02 18:29:36 +0200

    Update Cors

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
-    origin: '*',
-    methods: 'GET',
+    origin: 'https://manojlovic1998.github.io', // Replace with your GitHub Pages URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
   await app.listen(3000);
